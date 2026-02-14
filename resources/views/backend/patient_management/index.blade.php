cdd9d3c84e642816ae9cf001c7b7eb89a26b6d17
LabibArefin123 <ah947777@gmail.com>
2026-02-14 19:47:54 +0600

    import excel updated ui

--- resources/views/backend/patient_management/index.blade.php
+++ resources/views/backend/patient_management/index.blade.php
@@ -443,12 +443,31 @@
                         return;
                     }
 
-                    let url = $(this).attr('href');
+                    let baseUrl = $(this).attr('href');
+
+                    let params = {
+                        gender: $('select[name=gender]').val(),
+                        is_recommend: $('select[name=is_recommend]').val(),
+                        location_type: $('select[name=location_type]').val(),
+                        location_value: $('input[name=location_value]').val(),
+                        date_filter: $('select[name=date_filter]').val(),
+                        from_date: $('input[name=from_date]').val(),
+                        to_date: $('input[name=to_date]').val()
+                    };
+
+                    // Remove empty values
+                    Object.keys(params).forEach(key => {
+                        if (!params[key]) {
+                            delete params[key];
+                        }
+                    });
+
+                    let finalUrl = baseUrl + '?' + $.param(params);
 
                     showProcessModal(title, text, color);
 
                     setTimeout(function() {
-                        $('#downloadFrame').attr('src', url);
+                        $('#downloadFrame').attr('src', finalUrl);
                     }, 500);
 
                     setTimeout(function() {
@@ -456,6 +475,7 @@
                     }, 2500);
                 });
             }
+
 
 
             handleExport('.export-excel', 'Exporting Excel', 'Preparing Excel file...', '#28a745');
