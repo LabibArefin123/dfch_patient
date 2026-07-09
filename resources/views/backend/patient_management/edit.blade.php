9b7d6b9802e13017fd945d50e66acf928586f419
LabibArefin123 <ah947777@gmail.com>
2026-07-09 23:52:21 +0600

    added edit page, part 1 blade for patient info

--- resources/views/backend/patient_management/edit.blade.php
+++ resources/views/backend/patient_management/edit.blade.php
@@ -34,62 +34,7 @@
 
                 <div class="row">
 
-                    {{-- Patient Name --}}
-                    <div class="form-group col-md-6">
-                        <label>Patient's Name <span class="text-danger">*</span></label>
-                        <input type="text" name="patient_name" class="form-control"
-                            value="{{ old('patient_name', $patient->patient_name) }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label>Patient's Father Name<span class="text-danger">*</span></label>
-                        <input type="text" name="patient_f_name" class="form-control"
-                            value="{{ old('patient_f_name', $patient->patient_f_name) }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label>Patient's Mother Name<span class="text-danger">*</span></label>
-                        <input type="text" name="patient_m_name" class="form-control"
-                            value="{{ old('patient_m_name', $patient->patient_m_name) }}">
-                    </div>
-
-                    <div class="form-group col-md-3">
-                        <label>Age<span class="text-danger">*</span></label>
-                        <input type="number" name="age" class="form-control" value="{{ old('age', $patient->age) }}">
-                    </div>
-
-                    <div class="form-group col-md-3">
-                        <label>Gender<span class="text-danger">*</span></label>
-                        <select name="gender" class="form-control">
-                            <option value="">Select</option>
-                            <option value="male" {{ $patient->gender == 'male' ? 'selected' : '' }}>Male</option>
-                            <option value="female" {{ $patient->gender == 'female' ? 'selected' : '' }}>Female</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label>Patient's Phone 1 <span class="text-danger">*</span></label>
-                        <input type="text" name="phone_1" class="form-control"
-                            value="{{ old('phone_1', $patient->phone_1) }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label>Patient's Phone 2</label>
-                        <input type="text" name="phone_2" class="form-control"
-                            value="{{ old('phone_2', $patient->phone_2) }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label>Patient's Father Phone</label>
-                        <input type="text" name="phone_f_1" class="form-control"
-                            value="{{ old('phone_f_1', $patient->phone_f_1) }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label>Patient's Mother Phone</label>
-                        <input type="text" name="phone_m_1" class="form-control"
-                            value="{{ old('phone_m_1', $patient->phone_m_1) }}">
-                    </div>
+                    @include('backend.patient_management.partial_pages.edit_page.part_1')
 
                     {{-- Location --}}
                     <div class="form-group col-md-6">
